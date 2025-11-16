f3bd5c62db9b02dbc1423df6f0f110ce82a47650
Sadikshya <sadikshyamain02@gmail.com>
2025-11-16 10:49:22 +0545

    added a new about us page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Home = () => {
+  const name = "Sadikshya Nepal"
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-orange-500">
-        Welcome to Furever Home!
+        Welcome to Furever `${name}`!
       </h1>
     </div>
   );
